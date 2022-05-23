a24ddd04145890b59a95019bd86cf6dfff621d6e
Amit Kumar Yadav <amitk.dakshana16@gmail.com>
2022-05-23 10:47:17 +0530

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pli pair<ll, int>
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+
+using namespace std;
+
+const uint MXN = 100010;
+const uint MXA = 111;
+const uint MOD = 1000000007;
+const uint INF = 1000000007;
+
+void solve(){
+    
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    int t=1;
+    // cin>>t; 
+    while(t--){
+        solve();
+    }
+    return 0;
+}
